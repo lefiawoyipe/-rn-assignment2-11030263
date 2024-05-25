6b3dfe15526666f245153ae315601d79aad300ba
lefiawoyipe <lefiawoyipe@st.ug.edu.gh>
2024-05-25 16:41:44 +0000

    First commit

---
+++ my-app/App.js
@@ -0,0 +1,28 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  let name = "Elikem"
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>My name is <Text style= {styles.name} >{name}</Text> </Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "yellow",
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text:{
+    fontSize: 24,
+  },
+  name:{
+    fontWeight: "bold",
+  }
+
+});
